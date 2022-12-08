78fe687b3bc0db72b282c6c892fd562e08fee35b
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-08 18:01:16 +0100

    import themesetting,dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,24 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
+import Dashboard from "scenes/dashboard";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-    <ThemeProvider theme={theme}>
+   <BrowserRouter>
+      <ThemeProvider theme={theme}>
           <CssBaseline />
-    </ThemeProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              </Route>
+          </Routes>
+       </ThemeProvider>
+    </BrowserRouter>
     </div>
   )
 }
