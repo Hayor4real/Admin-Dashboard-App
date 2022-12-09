b0e61adbcbc3edbab7d09bda0a7a760044679b90
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-09 15:41:12 +0100

    add some code to navabar and passed the controller

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,9 +11,20 @@ import FlexBetween from "components/FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
 import profileImage from "assets/my picture.jpeg";
-import { AppBar, Toolbar, useTheme } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Box,
+  Typography,
+  IconButton,
+  InputBase,
+  Toolbar,
+  Menu,
+  MenuItem,
+  useTheme,
+} from "@mui/material";
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
   return (
